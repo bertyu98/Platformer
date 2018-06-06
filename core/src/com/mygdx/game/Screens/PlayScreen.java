ca9546640d7148603c23fe88bd5cf3f2e523bb90
Bert <yubb@uci.edu>
2018-06-06 01:24:09 -0700

    mario screen working

---
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -0,0 +1,71 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.MarioBros;
+import com.mygdx.game.Scenes.UI;
+
+public class PlayScreen implements Screen {
+
+    private MarioBros game;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+
+    private UI ui;
+
+    public PlayScreen(MarioBros game){
+        this.game = game;
+
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(MarioBros.V_WIDTH,MarioBros.V_WIDTH,gamecam);
+
+        ui = new UI(game.batch);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.setProjectionMatrix(ui.stage.getCamera().combined);
+        ui.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width,height);
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
